3de84f7940d342777d1247cb2fad739ad658a67a
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-07-01 17:49:36 -0600

    client setup & reactstrap

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Container,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink
+} from "reactstrap";
+
+class AppNavbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/bfulk13/MERN-shopping-list">
+                    GitHub
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;

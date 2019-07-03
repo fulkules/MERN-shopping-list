15a23ab5fde4a0d9f8fea80dde40c2f5a658eefd
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-07-03 12:17:06 -0600

    registration & logout

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,12 @@
 import axios from "axios";
-import { AUTH_ERROR, USER_LOADED, USER_LOADING } from "../actions/types";
+import {
+  AUTH_ERROR,
+  LOGOUT_SUCCESS,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+  USER_LOADING
+} from "../actions/types";
 import { returnErrors } from "./errorActions";
 
 // Check token and load user
@@ -21,6 +28,41 @@ export const loadUser = () => (dispatch, getState) => {
         type: AUTH_ERROR
       });
     });
+};
+
+// Register User
+export const register = ({ name, email, password }) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  // Request body
+  const body = JSON.stringify({ name, email, password });
+
+  axios
+    .post("/api/users", body, config)
+    .then(res =>
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, "REGISTER_FAIL")
+      );
+      dispatch({ type: REGISTER_FAIL });
+    });
+};
+
+// Logout User
+export const logout = () => {
+  return {
+    type: LOGOUT_SUCCESS
+  };
 };
 
 // Setup config/headers and token
